675d9033e9bfd92bcb9de4625445240d2a175b6b
EnesBaserr <enesbaser8184@gmail.com>
2024-01-24 22:52:00 +0300

    test

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706104334';
+const cacheName = 'chirpy-1706125792';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
