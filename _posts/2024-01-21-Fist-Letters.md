53b49bee435915fc418ab981bc98ce87f8e5a6cd
EnesBaserr <enesbaser8184@gmail.com>
2024-01-24 23:09:58 +0300

    new post added

---
+++ _posts/2024-01-21-Fist-Letters.md
@@ -0,0 +1,13 @@
+---
+title: First Letters
+
+categories: First Ones
+tags: First    # TAG names should always be lowercase
+---
+## INTRODUCTION
+
+- This is test for prod. 
+- Kocaeli
+- Kocaeli
+- Kocaeli
+- Kocaeli
--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706125792';
+const cacheName = 'chirpy-1706126966';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
